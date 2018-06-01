b502bc392adf577d675d9be50a47fcdd03088e38
Philip Hallstrom <phallstrom@instructure.com>
2018-06-01 14:22:15 -0700

    tweak about endpoint

--- spec/scorm_engine/api/endpoints/about_spec.rb
+++ spec/scorm_engine/api/endpoints/about_spec.rb
@@ -1,13 +1,13 @@
 RSpec.describe ScormEngine::Api::Endpoints::About do
   describe "#about" do
-    let(:subject) { ScormEngine::Client.new(tenant: "ScormEngineGemTesting").about }
+    let(:subject) { scorm_engine_client.about }
 
     it "is successful" do
       expect(subject.success?).to eq true
     end
 
     it "knows the version" do
-      expect(subject.result.version).to eq "2017.1.28.969"
+      expect(subject.result.version).to eq "2017.1.39.1184"
     end
 
     it "knows the platform" do
@@ -16,7 +16,7 @@ RSpec.describe ScormEngine::Api::Endpoints::About do
   end
 
   describe "#about_user_count" do
-    let(:subject) { ScormEngine::Client.new(tenant: "ScormEngineGemTesting").about_user_count }
+    let(:subject) { scorm_engine_client.about_user_count }
 
     it "is successful" do
       expect(subject.success?).to eq true
@@ -33,7 +33,7 @@ RSpec.describe ScormEngine::Api::Endpoints::About do
     it "tracks per tenantcounts" do
       aggregate_failures do
         expect(subject.result.by_tenant).to be_a Hash
-        tenant = subject.result.by_tenant.first.last
+        tenant = subject.result.by_tenant[scorm_engine_client.tenant.downcase]
         expect(tenant.total).to be >= 0
         expect(tenant.dispatched).to be >= 0
         expect(tenant.non_dispatched).to be >= 0
@@ -41,7 +41,7 @@ RSpec.describe ScormEngine::Api::Endpoints::About do
     end
 
     it "accepts :before option" do
-      subject = ScormEngine::Client.new(tenant: "ScormEngineGemTesting").about_user_count(before: Time.parse("1901-01-1 00:00:00 UTC"))
+      subject = scorm_engine_client.about_user_count(before: Time.parse("1901-01-1 00:00:00 UTC"))
       aggregate_failures do
         expect(subject.success?).to eq true
         expect(subject.result.total).to eq 0
@@ -49,7 +49,7 @@ RSpec.describe ScormEngine::Api::Endpoints::About do
     end
 
     it "accepts :since option" do
-      subject = ScormEngine::Client.new(tenant: "ScormEngineGemTesting").about_user_count(since: Time.parse("2031-01-1 00:00:00 UTC"))
+      subject = scorm_engine_client.about_user_count(since: Time.parse("2031-01-1 00:00:00 UTC"))
       aggregate_failures do
         expect(subject.success?).to eq true
         expect(subject.result.total).to eq 0
