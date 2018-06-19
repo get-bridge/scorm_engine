5c2ffed27f53e0a3dffe7c6a4256a39baa0ae539
Philip Hallstrom <phallstrom@instructure.com>
2018-06-19 13:54:04 -0700

    comment scorm engine spec helpers

--- spec/support/scorm_engine.rb
+++ spec/support/scorm_engine.rb
@@ -1,6 +1,11 @@
 module ScormEngineHelpers
   #
-  #
+  # If a real scorm engine is available this method will disable VCR and
+  # execute the passed block.  It is useful for when we need to ensure certain
+  # records are present in SCORM engine, but we do not want them recorded as
+  # the act of recording would break later expectations. That is if we record
+  # importing a course and play it back other tests that expect the actual
+  # record to exist in SCORM will fail.
   #
   def against_real_scorm_engine
     return unless scorm_engine_is_available?
@@ -13,14 +18,16 @@ module ScormEngineHelpers
   end
 
   #
-  #
+  # Check to see if a scorm engine is available. Probably true while developing
+  # locally, definitely not true in Travis.
   #
   def scorm_engine_is_available?
     ENV["SCORM_ENGINE_IS_AVAILABLE"] == "true"
   end
 
   #
-  #
+  # Ensure that the specified course exists in SCORM engine. Will import course
+  # if not present.
   #
   def ensure_course_exists(options = {})
     response = options[:client].courses(course_id: options[:course_id])
@@ -29,7 +36,7 @@ module ScormEngineHelpers
   end
 
   #
-  #
+  # Attempt to import a course to SCORM engine.
   #
   def import_course(options = {})
     options = {key: "RuntimeBasicCalls_SCORM20043rdEdition", may_create_new_version: true}.merge(options)
