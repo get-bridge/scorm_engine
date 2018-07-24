df26bd2e1af26afb984a66bbc51641527be66998
Philip Hallstrom <phallstrom@instructure.com>
2018-07-24 12:06:51 -0700

    set course configuration. refs #3

--- lib/scorm_engine/api/endpoints/courses.rb
+++ lib/scorm_engine/api/endpoints/courses.rb
@@ -138,7 +138,7 @@ module ScormEngine
         #
         # @returns [ScormEngine::Response]
         #
-        def course_configuration(options = {})
+        def get_course_configuration(options = {})
           course_id = options.delete(:course_id)
           raise ArgumentError.new('Required arguments :course_id missing') if course_id.nil?
           response = get("courses/#{course_id}/configuration", options)
@@ -148,6 +148,34 @@ module ScormEngine
           Response.new(raw_response: response, result: result)
         end
 
+        #
+        # Bulk set configuration settings via POST request.
+        #
+        # @see http://rustici-docs.s3.amazonaws.com/engine/2017.1.x/api.html#tenant__courses__courseId__configuration_post
+        #
+        # @param [Hash] options
+        #
+        # @option options [String] :course_id
+        #   The ID of the course to get.
+        #
+        # @option options [Integer] :version (nil)
+        #   The version of this course to use. If not provided, the latest
+        #   version will be used.
+        #
+        # @option options [Hash] :settings
+        #   Key/value pairs of configuration options to set.
+        #
+        # @returns [ScormEngine::Response]
+        #
+        def set_course_configuration(options = {})
+          course_id = options.delete(:course_id)
+          settings = options.delete(:settings)
+          raise ArgumentError.new('Required arguments :course_id missing') if course_id.nil?
+          raise ArgumentError.new('Required arguments :settings missing') if settings.nil?
+          body = {settings: settings.map { |k, v| { "settingId" => k, "value" => v.to_s } }}
+          response = post("courses/#{course_id}/configuration", options, body)
+          Response.new(raw_response: response)
+        end
 
         #
         # Import a course
--- spec/scorm_engine/api/endpoints/courses_spec.rb
+++ spec/scorm_engine/api/endpoints/courses_spec.rb
@@ -154,8 +154,8 @@ RSpec.describe ScormEngine::Api::Endpoints::Courses do
     end
   end
 
-  describe "#course_configuration" do
-    let(:response) { subject.course_configuration(course_id: "testing-golf-explained") }
+  describe "#get_course_configuration" do
+    let(:response) { subject.get_course_configuration(course_id: "testing-golf-explained") }
 
     it "is successful" do
       expect(response.success?).to eq true
@@ -173,10 +173,55 @@ RSpec.describe ScormEngine::Api::Endpoints::Courses do
     end
 
     it "fails when id is invalid" do
-      response = subject.course_configuration(course_id: "nonexistent-course")
+      response = subject.get_course_configuration(course_id: "nonexistent-course")
       expect(response.success?).to eq false
       expect(response.status).to eq 404
       expect(response.message).to match(/External Package ID 'nonexistent-course'/)
+    end
+  end
+
+  describe "#set_course_configuration" do
+    let(:response) { 
+      subject.set_course_configuration(
+        course_id: "testing-golf-explained", 
+        settings: {"PlayerCaptureHistoryDetailed" => "NO",
+                   "PlayerStatusRollupModeThresholdScore" => 80}
+      )
+    }
+
+    it "is successful" do
+      expect(response.success?).to eq true
+    end
+
+    it "persists the settings" do
+      response # trigger the setting
+      configuration = subject.get_course_configuration(course_id: "testing-golf-explained").result
+      expect(configuration.settings["PlayerCaptureHistoryDetailed"]).to eq "NO"
+      expect(configuration.settings["PlayerStatusRollupModeThresholdScore"]).to eq "80"
+
+      subject.set_course_configuration(
+        course_id: "testing-golf-explained", 
+        settings: {"PlayerCaptureHistoryDetailed" => "YES",
+                   "PlayerStatusRollupModeThresholdScore" => 42}
+      )
+
+      configuration = subject.get_course_configuration(course_id: "testing-golf-explained").result
+      expect(configuration.settings["PlayerCaptureHistoryDetailed"]).to eq "YES"
+      expect(configuration.settings["PlayerStatusRollupModeThresholdScore"]).to eq "42"
+    end
+
+    it "fails when id is invalid" do
+      response = subject.set_course_configuration(course_id: "nonexistent-course", settings: {})
+      expect(response.success?).to eq false
+      expect(response.status).to eq 404
+      expect(response.message).to match(/External Package ID 'nonexistent-course'/)
+    end
+
+    it "fails when settings are invalid" do
+      response = subject.set_course_configuration(course_id: "testing-golf-explained", settings: {"NonExistentSettingTotesBogus" => "YES"})
+      expect(response.success?).to eq false
+      expect(response.status).to eq 400
+      expect(response.message).to match(/No configuration setting found with id.*NonExistentSettingTotesBogus/)
     end
   end
 
