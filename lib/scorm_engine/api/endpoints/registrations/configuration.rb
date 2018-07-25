edfc2e97e508d8b8ed1d43e2fa8fb469e7542705
Philip Hallstrom <phallstrom@instructure.com>
2018-07-25 15:19:22 -0700

    registration configuration endpoints

--- lib/scorm_engine/api/endpoints/registrations/configuration.rb
+++ lib/scorm_engine/api/endpoints/registrations/configuration.rb
@@ -2,6 +2,127 @@ module ScormEngine
   module Api
     module Endpoints
       module Registrations::Configuration
+
+        #
+        # Returns the effective value of every setting at this level, as well
+        # as the effective value of any setting at a more specific level.
+        #
+        # @see http://rustici-docs.s3.amazonaws.com/engine/2017.1.x/api.html#tenant__registrations__registrationId__configuration_get
+        #
+        # @param [Hash] options
+        #
+        # @option options [String] :registration_id
+        #   The ID of the registration to get.
+        #
+        # @option options [Integer] :instance (nil)
+        #   The instance of this registration to use. If not provided, the
+        #   latest instance will be used.
+        #
+        # @return [ScormEngine::Models::RegistrationConfiguration]
+        #
+        def get_registration_configuration(options = {})
+          registration_id = options.delete(:registration_id)
+          raise ArgumentError.new('Required option :registration_id missing') if registration_id.nil?
+          response = get("registrations/#{registration_id}/configuration", options)
+          result = if response.success?
+                     ScormEngine::Models::RegistrationConfiguration.new_from_api(response.body)
+                   end
+          Response.new(raw_response: response, result: result)
+        end
+
+        #
+        # Bulk set configuration settings via POST request.
+        #
+        # @see http://rustici-docs.s3.amazonaws.com/engine/2017.1.x/api.html#tenant__registrations__registrationId__configuration_post
+        #
+        # @param [Hash] options
+        #
+        # @option options [String] :registration_id
+        #   The ID of the registration to set.
+        #
+        # @option options [Integer] :instance (nil)
+        #   The instance of this registration to use. If not provided, the
+        #   latest instance will be used.
+        #
+        # @option options [Hash] :settings
+        #   Key/value pairs of configuration options to set.
+        #
+        # @return [ScormEngine::Response]
+        #
+        def post_registration_configuration(options = {})
+          registration_id = options.delete(:registration_id)
+          settings = options.delete(:settings)
+          raise ArgumentError.new('Required option :registration_id missing') if registration_id.nil?
+          raise ArgumentError.new('Required option :settings missing') if settings.nil?
+          body = {settings: settings.map { |k, v| { "settingId" => k, "value" => v.to_s } }}
+          response = post("registrations/#{registration_id}/configuration", options, body)
+          Response.new(raw_response: response)
+        end
+
+        #
+        # Returns the effective value for this configuration setting for the resource being configured.
+        #
+        # @see http://rustici-docs.s3.amazonaws.com/engine/2017.1.x/api.html#tenant__registrations__registrationId__configuration__settingId__get
+        #
+        # @param [Hash] options
+        #
+        # @option options [String] :registration_id
+        #   The ID of the registration to get.
+        #
+        # @option options [String] :setting_id
+        #   The ID of the setting to get.
+        # 
+        # @option options [Integer] :instance (nil)
+        #   The instance of this registration to use. If not provided, the
+        #   latest instance will be used.
+        #
+        # @return [String]
+        #
+        def get_registration_configuration_setting(options = {})
+          registration_id = options.delete(:registration_id)
+          setting_id = options.delete(:setting_id)
+          raise ArgumentError.new('Required option :registration_id missing') if registration_id.nil?
+          raise ArgumentError.new('Required option :setting_id missing') if setting_id.nil?
+          response = get("registrations/#{registration_id}/configuration/#{setting_id}", options)
+          result = if response.success?
+                     response.body["value"]
+                   end
+          Response.new(raw_response: response, result: result)
+        end
+
+        #
+        # Sets the value for this configuration setting, for the resource being configured.
+        #
+        # @see http://rustici-docs.s3.amazonaws.com/engine/2017.1.x/api.html#tenant__registrations__registrationId__configuration__settingId__put
+        #
+        # @param [Hash] options
+        #
+        # @option options [String] :registration_id
+        #   The ID of the registration to set.
+        #
+        # @option options [String] :setting_id
+        #   The ID of the setting to set.
+        # 
+        # @option options [String] :value
+        #   The value of the setting to set.
+        # 
+        # @option options [Integer] :instance (nil)
+        #   The instance of this registration to use. If not provided, the
+        #   latest instance will be used.
+        #
+        # @return [ScormEngine::Response]
+        #
+        def put_registration_configuration_setting(options = {})
+          registration_id = options.delete(:registration_id)
+          setting_id = options.delete(:setting_id)
+          value = options.delete(:value)
+          raise ArgumentError.new('Required option :registration_id missing') if registration_id.nil?
+          raise ArgumentError.new('Required option :setting_id missing') if setting_id.nil?
+          raise ArgumentError.new('Required option :value missing') if value.nil?
+          body = {value: value.to_s}
+          response = put("registrations/#{registration_id}/configuration/#{setting_id}", options, body)
+          Response.new(raw_response: response)
+        end
       end
     end
   end
---
+++ spec/scorm_engine/api/endpoints/registrations/configuration_spec.rb
@@ -0,0 +1,154 @@
+RSpec.describe ScormEngine::Api::Endpoints::Registrations::Configuration do
+  let(:subject) { scorm_engine_client }
+
+  let(:registration_options) {{
+    course_id: "testing-golf-explained",
+    registration_id: "testing-golf-explained-registration-1",
+    learner: {
+      id: "testing-golf-explained-learner-1",
+      first_name: "Arnold",
+      last_name: "Palmer",
+    }
+  }}
+
+  before do
+    against_real_scorm_engine do
+      ensure_course_exists(client: subject, course_id: registration_options[:course_id])
+      ensure_registration_exists(registration_options.merge(client: subject))
+    end
+  end
+
+  describe "#get_registration_configuration" do
+    let(:response) { subject.get_registration_configuration(registration_id: registration_options[:registration_id]) }
+
+    it "is successful" do
+      expect(response.success?).to eq true
+    end
+
+    describe "results" do
+      it "makes settings available as key/value pairs" do
+        settings = response.result.settings
+        aggregate_failures do
+          # just a sampling
+          expect(settings.key?("PlayerStatusRollupModeValue")).to be_truthy
+          expect(settings.key?("PlayerLaunchType")).to be_truthy
+        end
+      end
+    end
+
+    it "fails when id is invalid" do
+      response = subject.get_registration_configuration(registration_id: "nonexistent-registration")
+      expect(response.success?).to eq false
+      expect(response.status).to eq 404
+      expect(response.message).to match(/External Registration ID 'nonexistent-registration'/)
+    end
+  end
+
+  describe "#post_registration_configuration" do
+    let(:response) { 
+      subject.post_registration_configuration(
+        registration_id: registration_options[:registration_id],
+        settings: {"PlayerCaptureHistoryDetailed" => "NO",
+                   "PlayerStatusRollupModeThresholdScore" => 80}
+      )
+    }
+
+    it "is successful" do
+      expect(response.success?).to eq true
+    end
+
+    it "persists the settings" do
+      response # trigger the api
+      configuration = subject.get_registration_configuration(registration_id: registration_options[:registration_id]).result
+      expect(configuration.settings["PlayerCaptureHistoryDetailed"]).to eq "NO"
+      expect(configuration.settings["PlayerStatusRollupModeThresholdScore"]).to eq "80"
+
+      subject.post_registration_configuration(
+        registration_id: registration_options[:registration_id],
+        settings: {"PlayerCaptureHistoryDetailed" => "YES",
+                   "PlayerStatusRollupModeThresholdScore" => 42}
+      )
+
+      configuration = subject.get_registration_configuration(registration_id: registration_options[:registration_id]).result
+      expect(configuration.settings["PlayerCaptureHistoryDetailed"]).to eq "YES"
+      expect(configuration.settings["PlayerStatusRollupModeThresholdScore"]).to eq "42"
+    end
+
+    it "fails when id is invalid" do
+      response = subject.post_registration_configuration(registration_id: "nonexistent-registration", settings: {})
+      expect(response.success?).to eq false
+      expect(response.status).to eq 404
+      expect(response.message).to match(/External Registration ID 'nonexistent-registration'/)
+    end
+
+    it "fails when settings are invalid" do
+      response = subject.post_registration_configuration(registration_id: registration_options[:registration_id], settings: {"NonExistentSettingTotesBogus" => "YES"})
+      expect(response.success?).to eq false
+      expect(response.status).to eq 400
+      expect(response.message).to match(/No configuration setting found with id.*NonExistentSettingTotesBogus/)
+    end
+  end
+
+  describe "#get_registration_configuration_setting" do
+    let(:response) { 
+      subject.put_registration_configuration_setting(registration_id: registration_options[:registration_id], setting_id: "PlayerStatusRollupModeThresholdScore", value: 42)
+      subject.get_registration_configuration_setting(registration_id: registration_options[:registration_id], setting_id: "PlayerStatusRollupModeThresholdScore")
+    }
+
+    it "is successful" do
+      expect(response.success?).to eq true
+    end
+
+    describe "results" do
+      it "returns the value as a string" do
+        expect(response.result).to eq "42"
+      end
+    end
+
+    it "fails when registration_id is invalid" do
+      response = subject.get_registration_configuration_setting(registration_id: "nonexistent-registration", setting_id: "PlayerStatusRollupModeThresholdScore")
+      expect(response.success?).to eq false
+      expect(response.status).to eq 404
+      expect(response.message).to match(/External Registration ID 'nonexistent-registration'/)
+    end
+
+    it "fails when setting_id is invalid" do
+      response = subject.get_registration_configuration_setting(registration_id: registration_options[:registration_id], setting_id: "NonExistentSettingTotesBogus")
+      expect(response.success?).to eq false
+      expect(response.status).to eq 400
+      expect(response.message).to match(/No configuration setting found with id.*NonExistentSettingTotesBogus/)
+    end
+  end
+
+  describe "#put_registration_configuration_setting" do
+    let(:value) { subject.get_registration_configuration_setting(registration_id: registration_options[:registration_id], setting_id: "PlayerStatusRollupModeThresholdScore").result }
+    let(:new_value) { (value.to_i + 1).to_s }
+    let(:response) { subject.put_registration_configuration_setting(registration_id: registration_options[:registration_id], setting_id: "PlayerStatusRollupModeThresholdScore", value: new_value) }
+
+    it "is successful" do
+      expect(response.success?).to eq true
+    end
+
+    describe "results" do
+      it "persists the changes" do
+        response # trigger the api
+        new_response = subject.get_registration_configuration_setting(registration_id: registration_options[:registration_id], setting_id: "PlayerStatusRollupModeThresholdScore")
+        expect(new_response.result).to eq new_value
+      end
+    end
+
+    it "fails when registration_id is invalid" do
+      response = subject.put_registration_configuration_setting(registration_id: "nonexistent-registration", setting_id: "PlayerStatusRollupModeThresholdScore", value: "42")
+      expect(response.success?).to eq false
+      expect(response.status).to eq 404
+      expect(response.message).to match(/External Registration ID 'nonexistent-registration'/)
+    end
+
+    it "fails when setting_id is invalid" do
+      response = subject.get_registration_configuration_setting(registration_id: registration_options[:registration_id], setting_id: "NonExistentSettingTotesBogus", value: "42")
+      expect(response.success?).to eq false
+      expect(response.status).to eq 400
+      expect(response.message).to match(/No configuration setting found with id.*NonExistentSettingTotesBogus/)
+    end
+  end
+end
