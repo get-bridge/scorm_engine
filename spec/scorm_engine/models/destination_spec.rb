1673a6c497c16f87b14d15260fb7b1c0c0d088b0
Philip Hallstrom <phallstrom@instructure.com>
2018-08-20 15:09:03 -0700

    oops

--- spec/scorm_engine/models/destination_spec.rb
+++ spec/scorm_engine/models/destination_spec.rb
@@ -2,19 +2,19 @@ RSpec.describe ScormEngine::Models::Destination do
   describe ".new_from_api" do
     describe ":name" do
       it "is set when passed at the root level" do
-        course = described_class.new_from_api(
+        destination = described_class.new_from_api(
           "name" => "test"
         )
-        expect(course.name).to eq "test"
+        expect(destination.name).to eq "test"
       end
 
       it "is set when passed at within `data`" do
-        course = described_class.new_from_api(
+        destination = described_class.new_from_api(
           "data" => {
             "name" => "test"
           }
         )
-        expect(course.name).to eq "test"
+        expect(destination.name).to eq "test"
       end
     end
   end
